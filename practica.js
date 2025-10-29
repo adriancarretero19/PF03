743ce4aa3fcfe1933f49073e7c5c39e3864ebc6e
yerko3 <yerkoclaros7@gmail.com>
2025-10-29 20:45:36 +0100

    funcion fibonacci GREEN

--- practica.js
+++ practica.js
@@ -1,3 +1,17 @@
 function fibonacci (n){
-    throw new Error("funcion vacia");
+    if(n <= 0)
+        throw new RangeError("solo números enteros ≥ 0");
+    if(n === 0)
+        return 0;
+    if (n === 1)
+        return 1;
+    
+    for (let i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return c;
+    //return fibonacci(n - 1) + fibonacci(n - 2);
 }
