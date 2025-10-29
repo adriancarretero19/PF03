ed4160d9be6b6989ab57e71749b583ea8a629e4d
yerko3 <yerkoclaros7@gmail.com>
2025-10-29 20:46:26 +0100

    funcion fibonacci REFACTOR

--- practica.js
+++ practica.js
@@ -5,13 +5,6 @@ function fibonacci (n){
         return 0;
     if (n === 1)
         return 1;
-    
-    for (let i = 2; i <= n; i++) {
-        c = a + b;
-        a = b;
-        b = c;
-    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 
-    return c;
-    //return fibonacci(n - 1) + fibonacci(n - 2);
 }
