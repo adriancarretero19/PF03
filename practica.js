86326062fbe268d717fa7a8562d9ab28a24455ec
adriancarretero19 <carreteromateoadrian@gmail.com>
2025-10-30 19:59:47 +0100

    funcion2 REFACTOR terminado

--- practica.js
+++ practica.js
@@ -1,30 +1,5 @@
 function esPinValido (pin){
-    if (pin == null)
+    if (pin == null || pin == undefined || typeof(pin) != "string" || (pin.length != 4 && pin.length != 6) || !/^\d+$/.test(pin) || /^(\d)\1+$/.test(pin))
         return false;
-
-    if (pin == undefined)
-        return false;
-
-    if (pin.length != 4 && pin.length != 6)
-        return false;
-    
-    if (typeof(pin) != "number")
-        return false;
-
-    for (let i = 1; i < pin.length; i++){
-        if (pin[0] != pin[i])
-            break;
-        if (i >= pin.length)
-            return false;
-    }
-
     return true;
 }
-
-/*
-▪ No puede ser null ni undefined.
-▪ No puede estar vacío.
-▪ Longitud 4 o 6 (típico de PINes).
-▪ Solo dígitos 0–9.
-▪ Debe prohibirse que todos los dígitos sean iguales (‘1111’, ‘888888’, ‘33333’…).
-*/
